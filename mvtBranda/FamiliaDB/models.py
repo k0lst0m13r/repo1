e85a845b93f982fb4203c0cfdfaf4706169c9dee
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-10 11:59:51 -0300

    modelo creado y hecha la migracion

--- mvtBranda/FamiliaDB/models.py
+++ mvtBranda/FamiliaDB/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Familiar(models.Model):
+    
+    parentezco = models.CharField(max_length=15)
+    nombre = models.CharField(max_length=15)
+    edad = models.IntegerField()
+    nacimiento = models.DateField()
+    
+    
+class Mascota(models.Model):
+    
+    nombre = models.CharField(max_length=15)
+    especie = models.CharField(max_length=15)
+    edad = models.IntegerField()
+    
