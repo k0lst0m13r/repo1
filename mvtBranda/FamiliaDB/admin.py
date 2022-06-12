7b3271a38719a4b5ec4216c6fde14d0eb921af0e
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-12 12:36:41 -0300

    creado superuser y cargada base de datos desde panel admin

--- mvtBranda/FamiliaDB/admin.py
+++ mvtBranda/FamiliaDB/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from FamiliaDB.models import *
 
 # Register your models here.
+class FamiliarAdmin(admin.ModelAdmin):
+    list_display = ('parentezco', 'nombre', 'edad', 'nacimiento')
+    
+class MascotaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'especie', 'edad')    
+
+admin.site.register(Familiar, FamiliarAdmin)
+admin.site.register(Mascota, MascotaAdmin)
