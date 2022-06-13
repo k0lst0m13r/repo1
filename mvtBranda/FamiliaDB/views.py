d341b45f8b4bd3478aa3959a48e642e927666749
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-12 21:21:19 -0300

    ya se ven los datos en el template

--- mvtBranda/FamiliaDB/views.py
+++ mvtBranda/FamiliaDB/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, HttpResponse
-
+from FamiliaDB.models import *
 # Create your views here.
 
 def index(request):
-    return render(request, 'FamiliaDB/index.html')
+    
+    parentezco = [x.parentezco for x in Familiar.objects.all()]
+    nombre = [x.nombre for x in Familiar.objects.all()]
+    edad = [x.edad for x in Familiar.objects.all()]
+    nacimiento = [x.nacimiento for x in Familiar.objects.all()]
+    
+    especie = [x.especie for x in Mascota.objects.all()]
+    nombre_mascota = [x.nombre for x in Mascota.objects.all()]
+    
+    return render(request, 'FamiliaDB/index.html', {"parentezco": parentezco, "nombre": nombre, "edad": edad, "nacimiento": nacimiento, "especie":especie, "nombre_mascota":nombre_mascota})
+   
